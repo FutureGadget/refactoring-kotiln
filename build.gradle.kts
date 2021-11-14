2d4e390516a75d9eee0b8d5f577c230c9117ff61
danwoo.park <danwoopark@gmail.com>
2021-11-14 13:55:34 +0900

    jacoco

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,8 @@
-import org.jetbrains.kotlin.cli.jvm.compiler.findMainClass
-
 plugins {
     kotlin("jvm") version "1.5.31"
     idea
     application
+    jacoco
 }
 
 group "org.example"
@@ -22,6 +21,10 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+    extensions.configure(JacocoTaskExtension::class) {
+        setDestinationFile(file("${buildDir}/jacoco/test.exec"))
+    }
+    finalizedBy(tasks.jacocoTestReport)
 }
 
 tasks.compileKotlin {
@@ -38,4 +41,59 @@ tasks.compileTestKotlin {
 
 application {
     mainClass.set("PerformanceBillingSystemKt")
+}
+
+jacoco {
+    toolVersion = "0.8.7"
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+        csv.required.set(true)
+    }
+    finalizedBy(tasks.jacocoTestCoverageVerification)
+}
+
+// 커버리지 검증
+tasks.jacocoTestCoverageVerification {
+    violationRules {
+        rule {
+            limit {
+                minimum = "0.20".toBigDecimal()
+            }
+        }
+
+        rule {
+// 룰을 간단히 켜고 끌 수 있다.
+            enabled = true
+
+// 룰을 체크할 단위는 클래스 단위
+            element = "CLASS"
+
+// 브랜치 커버리지를 최소한 90% 만족시켜야 한다.
+            limit {
+                counter = "BRANCH"
+                value = "COVEREDRATIO"
+                minimum = "0.90".toBigDecimal()
+            }
+
+// 라인 커버리지를 최소한 80% 만족시켜야 한다.
+            limit {
+                counter = "LINE"
+                value = "COVEREDRATIO"
+                minimum = "0.80".toBigDecimal()
+            }
+
+// 빈 줄을 제외한 코드의 라인수를 최대 200라인으로 제한한다.
+            limit {
+                counter = "LINE"
+                value = "TOTALCOUNT"
+                maximum = "200".toBigDecimal()
+            }
+
+// 커버리지 체크를 제외할 클래스들
+            excludes = listOf()
+        }
+    }
 }
